1ba2024f0ea3b9b9c4bf05cfea33945b748694d0
Okapi <svmzh15@gmail.com>
2024-07-19 23:02:41 +0300

    Removed unused imports and RateDTO class.

--- src/main/java/com/sadon1on/exchangerateservice/ExchangeRateServiceApplication.java
+++ src/main/java/com/sadon1on/exchangerateservice/ExchangeRateServiceApplication.java
@@ -1,8 +1,5 @@
 package com.sadon1on.exchangerateservice;
 
-import com.sadon1on.exchangerateservice.currency.NationalBankRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/sadon1on/exchangerateservice/rate/ExchangeRateRepository.java
+++ src/main/java/com/sadon1on/exchangerateservice/rate/ExchangeRateRepository.java
@@ -3,7 +3,6 @@ package com.sadon1on.exchangerateservice.rate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Repository
